fc0fe47845fd6e21f9ddc920aff70e8a64b7858b
Salompablo <pablosalompita@gmail.com>
2022-05-04 16:21:23 +0200

    Add base-proyect templates and create header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPrAewL8daNpL23Mg_31mWED1zgSPPDbk",
+    authDomain: "ecommerce-95c13.firebaseapp.com",
+    projectId: "ecommerce-95c13",
+    storageBucket: "ecommerce-95c13.appspot.com",
+    messagingSenderId: "1060065231720",
+    appId: "1:1060065231720:web:9501eb72c341fa95e5ccdd",
+    measurementId: "G-61G51L4JTM"
+};
+
