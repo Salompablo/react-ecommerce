1262210df1af578037e8e53b5d682dca11919a62
Salompablo <pablosalompita@gmail.com>
2022-06-03 19:06:05 +0200

    Add searchbar icons and nav bar links style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-import Header from "./components/Header/Header";
+import './App.css';
+import Header from './components/Home/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+    <Header />
     </div>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAPrAewL8daNpL23Mg_31mWED1zgSPPDbk",
-    authDomain: "ecommerce-95c13.firebaseapp.com",
-    projectId: "ecommerce-95c13",
-    storageBucket: "ecommerce-95c13.appspot.com",
-    messagingSenderId: "1060065231720",
-    appId: "1:1060065231720:web:9501eb72c341fa95e5ccdd",
-    measurementId: "G-61G51L4JTM"
-};
-
+    apiKey: "AIzaSyDFlOwiqE9I89szqGzlCRlA1B_7zlK66Xs",
+    authDomain: "commerce-c0e37.firebaseapp.com",
+    projectId: "commerce-c0e37",
+    storageBucket: "commerce-c0e37.appspot.com",
+    messagingSenderId: "248628935219",
+    appId: "1:248628935219:web:453e08c92b9dd56d8b8146",
+    measurementId: "G-QQVK8PC29P"
+  };
